2928ded5213bc62d6852e95c31bcdc151d8bd4ff
erkihindo <erkihindo@gmail.com>
2018-12-28 15:40:28 +0200

    added specialities

---
+++ pet-clinic-data/src/main/java/erki/springframework/sfgpetclinicerki/model/Speciality.java
@@ -0,0 +1,14 @@
+package erki.springframework.sfgpetclinicerki.model;
+
+public class Speciality {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
