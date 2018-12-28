5a368d854b7845f01909cd15533f8c4b2f394037
erkihindo <erkihindo@gmail.com>
2018-12-28 15:49:22 +0200

    Added pettype service

---
+++ pet-clinic-data/src/main/java/erki/springframework/sfgpetclinicerki/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package erki.springframework.sfgpetclinicerki.services;
+
+import erki.springframework.sfgpetclinicerki.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>  {
+
+}
---
+++ pet-clinic-data/src/main/java/erki/springframework/sfgpetclinicerki/services/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package erki.springframework.sfgpetclinicerki.services.map;
+
+import erki.springframework.sfgpetclinicerki.model.PetType;
+import erki.springframework.sfgpetclinicerki.services.PetTypeService;
+import java.util.Set;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
